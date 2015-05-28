698e8dac276735fe3087116b6401032a3d84be18
Ian Outterside <ianoshorty@gmail.com>
2015-05-29 00:23:01 +0100

    Adding file output in phpunit with --debug command

--- tests/TestFixture.php
+++ tests/TestFixture.php
@@ -34,4 +34,17 @@ class TestFixture extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf('Axel\Axel', $d);
     }
+
+    protected function log($output, $message = null) {
+
+        if(in_array('--debug', $_SERVER['argv'], true)) {
+            fwrite(STDERR, '========================' . "\n");
+            if (is_string($message)) {
+                fwrite(STDERR, $message . "\n\n");
+            }
+
+            fwrite(STDERR, print_r($output, TRUE));
+            fwrite(STDERR, '========================' . "\n\n");
+        }
+    }
 }
